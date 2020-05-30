5ea5b481293dc55c6641f90d1d74a5acd8e24bb5
martin <zhengxi.li.19@neoma-bs.com>
2020-05-30 23:14:27 +0200

    完成注册及重置密码链接显示页面

--- src/app/modules/account/pages/page-email-sended/page-email-sended.component.ts
+++ src/app/modules/account/pages/page-email-sended/page-email-sended.component.ts
@@ -5,34 +5,33 @@ import { LoginService } from 'src/app/login.service';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-email-sended',
-    templateUrl: './page-email-sended.component.html',
-    styleUrls: ['./page-email-sended.component.scss']
+  selector: 'app-email-sended',
+  templateUrl: './page-email-sended.component.html',
+  styleUrls: ['./page-email-sended.component.scss']
 })
 export class PageEmailSendedComponent {
-    public Message:string ="";
+  public Message: string = "";
 
-    constructor( 
-        private loginService : LoginService, 
-        private router: Router,
-        private route: ActivatedRoute,
-        private translateService: TranslateService) { }
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private translateService: TranslateService) { }
 
-    ngOnInit(): void {
-      this.route.queryParams.subscribe(params=>{
-          if(params!=null && params.Email!=null && params.Type!=null){
-              if(params.Type == 'Registre'){
-                this.Message = this.translateService.instant('SendMessageTo').replace('{Email}',params.Email);
-              }
-              else if(params.Type == 'ResetPassword'){
-                this.Message = this.translateService.instant('SendMessageTo').replace('{Email}',params.Email);
-              }
-            
-          }
-          else{
-              this.router.navigate(['/']);
-          }
-      })
-        
-    }
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (params != null && params.Email != null && params.Type != null) {
+        if (params.Type == 'Registre') {
+          this.Message = this.translateService.instant('forget-password.registersuccessed').replace('{Email}', params.Email);
+        }
+        else if (params.Type == 'ResetPassword') {
+          this.Message = this.translateService.instant('forget-password.SendMessageTo').replace('{Email}', params.Email);
+        }
+      }
+      else {
+        this.router.navigate(['/']);
+      }
+    })
+
+  }
 }
